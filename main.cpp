bbdd9928c876831fd6592fe75f6d3bf4db071c4d
Jeffrey Chen <ultimatedbz@jeffreys-imac.lan>
2020-11-26 00:10:53 -0500

    3.0

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     Dialog w;
-    w.setWindowTitle("Inventory 2.0");
+    w.setWindowTitle("Inventory 3.0");
     w.show();
 
     return a.exec();
